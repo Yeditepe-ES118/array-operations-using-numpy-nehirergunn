c2b5f69b0a5cd306e0765372f6cf54ec0b5b3169
nehirrgunn <nehir.ergun@std.yeditepe.edu.tr>
2026-01-02 18:07:04 +0300

    assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -10,15 +10,25 @@ import numpy as np
 def stat():
     data = np.loadtxt("populations.txt")
 
+    # columns: Year, Hare, Lynx, Carrot
+    year = data[:, 0]
     hare = data[:, 1]
+    lynx = data[:, 2]
+    carrot = data[:, 3]
 
-    min_year_hare = np.argmin(hare)
+    # year where hare is minimum (actual year, not index)
+    min_year_hare = year[np.argmin(hare)]
 
-    lynx_avg = np.mean(data[:, 2])
+    # average of lynx
+    lynx_avg = np.mean(lynx)
 
-    total = np.sum(data, axis=1)
+    # total of species ONLY (exclude year)
+    total = hare + lynx + carrot
+
+    # add last column
     new_data = np.column_stack((data, total))
 
+    # set carrot < 40000 to 0 (in new_data)
     new_data[new_data[:, 3] < 40000, 3] = 0
 
     return data, hare, min_year_hare, lynx_avg, new_data
